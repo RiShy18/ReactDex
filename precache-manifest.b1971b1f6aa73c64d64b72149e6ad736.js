8f23c25d6822a8bcd5039ce7a1a9f956dcc3448d
RiShy18 <molinari@GAFK43HBKU.americas.hpqcorp.net>
2022-04-26 16:11:47 -0600

    Updates

---
+++ precache-manifest.b1971b1f6aa73c64d64b72149e6ad736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd818724131b3e98d28d",
+    "url": "/ReactDex/static/css/main.9aacae82.chunk.css"
+  },
+  {
+    "revision": "dd818724131b3e98d28d",
+    "url": "/ReactDex/static/js/main.dd818724.chunk.js"
+  },
+  {
+    "revision": "65460345ea1c43b95178",
+    "url": "/ReactDex/static/css/1.8d221a89.chunk.css"
+  },
+  {
+    "revision": "65460345ea1c43b95178",
+    "url": "/ReactDex/static/js/1.65460345.chunk.js"
+  },
+  {
+    "revision": "bf8c3a743c6b72ed2dd3",
+    "url": "/ReactDex/static/js/runtime~main.bf8c3a74.js"
+  },
+  {
+    "revision": "93754e26133bf7121b03b55b63ae3584",
+    "url": "/ReactDex/index.html"
+  }
+];
